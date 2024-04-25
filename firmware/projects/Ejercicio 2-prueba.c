916c502ed00c4199ee44e8dd3653f54f899d5ede
GiovannaVinoli <gvinolimiotti@ingenieria.uner.edu.ar>
2024-04-25 09:39:51 -0300

    Se sube ejercicio 3 funcionando con las implementaciones de las teclas I y M. Falta subir documentación. Se crea proyecto para el ejercicio 4

--- firmware/projects/Ejercicio 2-prueba.c
+++ firmware/projects/Ejercicio 2-prueba.c
@@ -1,5 +1,5 @@
-/*! @mainpage Actividad 2 Proyecto: Medidor de distancia por ultrasonido con interrupciones
- *
+Ejercicio 2-prueba
+/* *
  * @section genDesc General Description
  * Se utiliza el medidor de distancia con un sensor ultrasónico de la actividad anterior, donde 
  * las funcionalidades de las teclas 1 y 2 de la placa, se implementan como interrupciones y las
@@ -28,6 +28,12 @@
  * | +5V			|		+5V
  * | GND			|		GND
  *
+ * |    ESP32c6	  |   Periférico   	|
+ * |:--------------:|:--------------|
+ * | 			|	
+ * | 			|  
+ * |			|   
+ * |	 		|	
  *	
  *
  *
@@ -35,7 +41,7 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * |:12/04/2024:|: Generación de documentación					 |
+ * |:12/04/2024:|: Generación del proyecto					 |
  *
  * @author Giovanna Viñoli (giovanna.vinoli@uner.edu.ar)
  *
@@ -52,6 +58,8 @@
 #include "lcditse0803.h"
 #include "switch.h"
 #include "timer_mcu.h"
+#include "uart_mcu.h"
+#include "ctype.h"
 
 /*==================[macros and definitions]=================================*/
 /// defino el tiempo (en microsegundos) entre las interrupciones del timer. 
@@ -61,8 +69,11 @@
 /**
  * Defino variables globales
 */
-bool activar = true, congelar = false;
-float distancia;
+bool activar = true, congelar = false, pulgadas = false, ver_maximo = false;
+uint16_t distancia, maximo=0;
+uint16_t distancia_min_cm = 10, distancia_media_cm =20, distamcia_max_cm = 30;
+uint16_t distancia_min_in = 10/2.54, distancia_media_in = 20/2.54, distamcia_max_in =30/2.54;
+
 /*==================[internal data definition]===============================*/
 /// tipo (struct) de FREERTOS utilizado para hacer referencia a las tareas y así
 /// usar las tareas (a partir de estar referidas por estos TaskHandle) en otros 
@@ -70,6 +81,7 @@ float distancia;
 TaskHandle_t sensar_task_handle = NULL;
 TaskHandle_t leds_task_handle = NULL;
 TaskHandle_t display_task_handle = NULL;
+TaskHandle_t MandarDarosUART_handle = NULL;
 
 /*==================[internal functions declaration]=========================*/
 /**
@@ -83,6 +95,63 @@ void FuncionTimer(void* param){
     vTaskNotifyGiveFromISR(leds_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada a los leds*/
 	vTaskNotifyGiveFromISR(sensar_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al sensor*/
     vTaskNotifyGiveFromISR(display_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al display */
+	vTaskNotifyGiveFromISR(MandarDarosUART_handle, pdFALSE);
+}
+
+void FuncionUART(void* param){
+	uint8_t uart;
+	UartReadByte(UART_PC, &uart);
+	uart = tolower(uart);
+	
+	switch (uart)
+	{
+	case 'O':
+		cambiarEstado(activar);
+		//activar=! activar;
+		break;
+	
+	case 'H':
+		cambiarEstado(congelar);
+		//congelar =! congelar;
+		break;
+
+	case 'I':
+		cambiarEstado(pulgadas);
+		//pulgadas =! pulgadas;
+		break;
+
+	case 'M':
+		cambiarEstado(ver_maximo);
+		//ver_maximo =! ver_maximo;
+		break;
+	}
+}
+
+
+static void MandarDatosUART(void* param){
+	while(true){ // agregarle la condicion de si esta apagado o prendido, agregarle los cm. y un \n
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		if(activar == true){
+			if(ver_maximo == false){
+				if(pulgadas == false){
+					UartSendString(UART_PC,(char*) UartItoa(distancia, 10));
+					UartSendString(UART_PC, " in\n");
+				}
+				else{
+					UartSendString(UART_PC,(char*) UartItoa(distancia, 10));
+					UartSendString(UART_PC, " cm\n");
+				}
+			}
+			else{
+				UartSendString(UART_PC, " El valor máximo en cm es: ");
+				UartSendString(UART_PC,(char*) UartItoa(maximo, 10));
+				UartSendString(UART_PC, "\n");
+
+			}
+
+		}
+		
+	}//pyserial ver 
 }
 
 /**
@@ -100,9 +169,20 @@ static void SensarTask(void *pvParameter){
 		/// ejecutarse
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* Envía una notificación a la tarea asociada al LED_2 */
 		/// a partir de acá defino lo que quiero que haga mi función SensarTask
-		printf("Sensando\n");
+		//printf("Sensando\n");
 		if(activar == true){
-			distancia = HcSr04ReadDistanceInCentimeters();
+			if(pulgadas == false){
+				distancia = HcSr04ReadDistanceInCentimeters();
+				if(distancia > maximo){
+					maximo = distancia;
+				}
+			}
+			else{
+				distancia = HcSr04ReadDistanceInInches();
+				if((distancia*2.54) > maximo){
+					maximo = (distancia*2.54);
+				}
+			}
 		}
 	}
 }
@@ -110,29 +190,39 @@ static void SensarTask(void *pvParameter){
  * @fn static void LedsTask(void *pvParameter)
  * @brief función que enciende y apaga los leds dependiendo el valor de la distancia medida por el sensor
 */
+
+void configurarLeds(uint16_t distancia_min, uint16_t distancia_media, uint16_t distancia_max){
+	if(distancia <= distancia_min){
+		LedsOffAll();
+	}	
+	else if( distancia <= distancia_media && distancia >distancia_min){
+		LedOn(LED_1);
+		LedOff(LED_2);
+		LedOff(LED_3);
+	}
+	else if(distancia <= distancia_max && distancia > distancia_media){
+		LedOn(LED_1);
+		LedOn(LED_2);
+		LedOff(LED_3);
+	}
+	else if (distancia > distancia_max){
+		LedOn(LED_1);
+		LedOn(LED_2);
+		LedOn(LED_3);
+	}
+}
+
 static void LedsTask(void *pvParameter){
 	//el portMAXdelay te dice que espere infinito por la notificación. Yo podría decirle, che, espera 1 segundo por la notificaicón
 	while(true){
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* Envía una notificación a la tarea asociada al LED_2 */
-		printf("Leds\n");
 		if(activar == true){
-			if(distancia <= 10){
-				LedsOffAll();
-			}	
-			else if( distancia <= 20 && distancia >10){
-				LedOn(LED_1);
-				LedOff(LED_2);
-				LedOff(LED_3);
+			//printf("%i\n", distancia);
+			if(pulgadas == false){
+				configurarLeds(distancia_min_cm, distancia_media_cm, distamcia_max_cm);
 			}
-			else if(distancia <= 30 && distancia > 20){
-				LedOn(LED_1);
-				LedOn(LED_2);
-				LedOff(LED_3);
-			}
-			else if (distancia > 30){
-				LedOn(LED_1);
-				LedOn(LED_2);
-				LedOn(LED_3);
+			else{
+				configurarLeds(distancia_min_in, distancia_media_in, distamcia_max_in);
 			}
 		}
 		else {
@@ -168,7 +258,7 @@ estado de activar y congelar respectivamente
 */
 //creo las dos funciones que serán llamadas cuando se den las interrupciones
 
-static void FuncionSwitch(void *pvParam){
+static void cambiarEstado(void *pvParam){
 	bool *puntero= (bool*)(pvParam);
 	*puntero =! *puntero;
 }
@@ -183,8 +273,11 @@ void app_main(void){
 	SwitchesInit();
 	/// defino que cuando se presiona los switch, que se ejecute determinada función, y le mando 
 	/// otro parámetro
-	SwitchActivInt(SWITCH_1,FuncionSwitch, &activar);
-	SwitchActivInt(SWITCH_2,FuncionSwitch, &congelar);
+	SwitchActivInt(SWITCH_1,cambiarEstado, &activar);
+	SwitchActivInt(SWITCH_2,cambiarEstado, &congelar);
+
+	// Congifuracion de puertos:
+	//GPIOInit(pin , dirección: input u  output);
 
 	/* Definición del timer: lo que me hace definir más de un timer es cuando quiero trabajar con diferntes bases de tiempo.
 	Como ahora tengo la misma base de tiempo que es 1 segundo = 1000000 microsegundos, voy a crear solo uno para las mismas tareas*/
@@ -196,14 +289,24 @@ void app_main(void){
 		/// línea anterior. 
         .param_p = NULL
     };
-
-	/// Inicialización del timer
+		/// Inicialización del timer
 	TimerInit(&timer_A);
+
+	serial_config_t Uart = {
+		.port = UART_PC,
+		.baud_rate = 9600,
+		.func_p = FuncionUART,
+		.param_p = NULL
+	};
+	/// Inicializo el UART
+	UartInit(&Uart);
+
 
 	/// creación de las tareas que quiero ejecutar 
 	xTaskCreate(&SensarTask, "Sensar", 512, NULL, 4, &sensar_task_handle);
 	xTaskCreate(&LedsTask, "Leds", 512, NULL, 4, &leds_task_handle);
 	xTaskCreate(&DisplayTask, "Display", 512, NULL, 4, &display_task_handle);
+	xTaskCreate(&MandarDatosUART, "UART", 512, NULL, 4, &MandarDarosUART_handle);
 
 	/// línea donde defino que comienza a funcionar el timer. En este caso, timer_A.timer me devuelve TIMER_A
 	/// porque es de la estructura definida en la línea 179, el primer ítem
--- firmware/projects/guia2_ej3.c
+++ firmware/projects/guia2_ej3.c
@@ -28,6 +28,12 @@
  * | +5V			|		+5V
  * | GND			|		GND
  *
+ * |    ESP32c6	  |   Periférico   	|
+ * |:--------------:|:--------------|
+ * | 			|	
+ * | 			|  
+ * |			|   
+ * |	 		|	
  *	
  *
  *
@@ -35,7 +41,7 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * |:12/04/2024:|: Generación de documentación					 |
+ * |:12/04/2024:|: Generación del proyecto					 |
  *
  * @author Giovanna Viñoli (giovanna.vinoli@uner.edu.ar)
  *
@@ -52,6 +58,8 @@
 #include "lcditse0803.h"
 #include "switch.h"
 #include "timer_mcu.h"
+#include "uart_mcu.h"
+#include "ctype.h"
 
 /*==================[macros and definitions]=================================*/
 /// defino el tiempo (en microsegundos) entre las interrupciones del timer. 
@@ -61,8 +69,11 @@
 /**
  * Defino variables globales
 */
-bool activar = true, congelar = false;
-float distancia;
+bool activar = true, congelar = false, pulgadas = false, ver_maximo = false;
+uint16_t distancia;
+uint16_t distancia_min_cm = 10, distancia_media_cm =20, distamcia_max_cm = 30;
+uint16_t distancia_min_in = 10/2.54, distancia_media_in = 20/2.54, distamcia_max_in =30/2.54;
+
 /*==================[internal data definition]===============================*/
 /// tipo (struct) de FREERTOS utilizado para hacer referencia a las tareas y así
 /// usar las tareas (a partir de estar referidas por estos TaskHandle) en otros 
@@ -70,6 +81,7 @@ float distancia;
 TaskHandle_t sensar_task_handle = NULL;
 TaskHandle_t leds_task_handle = NULL;
 TaskHandle_t display_task_handle = NULL;
+TaskHandle_t MandarDarosUART_handle = NULL;
 
 /*==================[internal functions declaration]=========================*/
 /**
@@ -83,6 +95,69 @@ void FuncionTimer(void* param){
     vTaskNotifyGiveFromISR(leds_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada a los leds*/
 	vTaskNotifyGiveFromISR(sensar_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al sensor*/
     vTaskNotifyGiveFromISR(display_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al display */
+	vTaskNotifyGiveFromISR(MandarDarosUART_handle, pdFALSE);
+}
+/* para manejar los switches, a diferencia del ejercicio anterior, debo cambiar la forma de implementarlo. 
+Ya no voy a revisar qué switch es el que se presiona. Ahora voy a configurar directamente cada switch por 
+separado y con la función de la librería switch, "SwitchActivInt" le voy a decir qué tecla se apretó y 
+qué función debe ejecutar cuando esto sucede:
+1) creo dos funciones, "FuncionSwitch1" y "FuncionSwitch2" en donde adentro de la funcion cambio el 
+estado de activar y congelar respectivamente
+2) creo una funcion genérica, en donde le paso por parámetro en la funcion de SwitchActivInt(-- , -- , xx)
+(donde se encuentra la xx) y le paso cuál tendré que modificar: activar o congelar
+*/
+//creo las dos funciones que serán llamadas cuando se den las interrupciones
+
+static void cambiarEstado(bool *pvParam){
+	bool *puntero= (bool*)(pvParam);
+	*puntero =! *puntero;
+}
+
+void FuncionUART(void* param){
+	uint8_t uart;
+	UartReadByte(UART_PC, &uart);
+	uart = toupper(uart);
+	
+	switch (uart)
+	{
+		case 'O':
+			cambiarEstado(&activar);
+			//activar=! activar;
+			break;
+		
+		case 'H':
+			cambiarEstado(&congelar);
+			//congelar =! congelar;
+			break;
+
+		case 'I':
+			cambiarEstado(&pulgadas);
+			//pulgadas =! pulgadas;
+			break;
+
+		case 'M':
+			cambiarEstado(&ver_maximo);
+			//ver_maximo =! ver_maximo;
+			break;
+	}
+}
+
+
+static void MandarDatosUART(void* param){
+	while(true){ // agregarle la condicion de si esta apagado o prendido, agregarle los cm. y un \n
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		if(activar == true){
+			if(pulgadas == true){
+				UartSendString(UART_PC,(char*) UartItoa(distancia, 10));
+				UartSendString(UART_PC, " in\n");
+			}
+			else{
+				UartSendString(UART_PC,(char*) UartItoa(distancia, 10));
+				UartSendString(UART_PC, " cm\n");
+			}
+		}
+		
+	}//pyserial ver 
 }
 
 /**
@@ -100,9 +175,14 @@ static void SensarTask(void *pvParameter){
 		/// ejecutarse
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* Envía una notificación a la tarea asociada al LED_2 */
 		/// a partir de acá defino lo que quiero que haga mi función SensarTask
-		printf("Sensando\n");
+		//printf("Sensando\n");
 		if(activar == true){
-			distancia = HcSr04ReadDistanceInCentimeters();
+			if(pulgadas == false){
+				distancia = HcSr04ReadDistanceInCentimeters();
+			}
+			else{
+				distancia = HcSr04ReadDistanceInInches();
+			}
 		}
 	}
 }
@@ -110,29 +190,39 @@ static void SensarTask(void *pvParameter){
  * @fn static void LedsTask(void *pvParameter)
  * @brief función que enciende y apaga los leds dependiendo el valor de la distancia medida por el sensor
 */
+
+void configurarLeds(uint16_t distancia_min, uint16_t distancia_media, uint16_t distancia_max){
+	if(distancia <= distancia_min){
+		LedsOffAll();
+	}	
+	else if( distancia <= distancia_media && distancia >distancia_min){
+		LedOn(LED_1);
+		LedOff(LED_2);
+		LedOff(LED_3);
+	}
+	else if(distancia <= distancia_max && distancia > distancia_media){
+		LedOn(LED_1);
+		LedOn(LED_2);
+		LedOff(LED_3);
+	}
+	else if (distancia > distancia_max){
+		LedOn(LED_1);
+		LedOn(LED_2);
+		LedOn(LED_3);
+	}
+}
+
 static void LedsTask(void *pvParameter){
 	//el portMAXdelay te dice que espere infinito por la notificación. Yo podría decirle, che, espera 1 segundo por la notificaicón
 	while(true){
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* Envía una notificación a la tarea asociada al LED_2 */
-		printf("Leds\n");
 		if(activar == true){
-			if(distancia <= 10){
-				LedsOffAll();
-			}	
-			else if( distancia <= 20 && distancia >10){
-				LedOn(LED_1);
-				LedOff(LED_2);
-				LedOff(LED_3);
+			//printf("%i\n", distancia);
+			if(pulgadas == false){
+				configurarLeds(distancia_min_cm, distancia_media_cm, distamcia_max_cm);
 			}
-			else if(distancia <= 30 && distancia > 20){
-				LedOn(LED_1);
-				LedOn(LED_2);
-				LedOff(LED_3);
-			}
-			else if (distancia > 30){
-				LedOn(LED_1);
-				LedOn(LED_2);
-				LedOn(LED_3);
+			else{
+				configurarLeds(distancia_min_in, distancia_media_in, distamcia_max_in);
 			}
 		}
 		else {
@@ -157,21 +247,7 @@ static void DisplayTask(void *pvParameter){
 		} 
 	}
 }
-/* para manejar los switches, a diferencia del ejercicio anterior, debo cambiar la forma de implementarlo. 
-Ya no voy a revisar qué switch es el que se presiona. Ahora voy a configurar directamente cada switch por 
-separado y con la función de la librería switch, "SwitchActivInt" le voy a decir qué tecla se apretó y 
-qué función debe ejecutar cuando esto sucede:
-1) creo dos funciones, "FuncionSwitch1" y "FuncionSwitch2" en donde adentro de la funcion cambio el 
-estado de activar y congelar respectivamente
-2) creo una funcion genérica, en donde le paso por parámetro en la funcion de SwitchActivInt(-- , -- , xx)
-(donde se encuentra la xx) y le paso cuál tendré que modificar: activar o congelar
-*/
-//creo las dos funciones que serán llamadas cuando se den las interrupciones
 
-static void FuncionSwitch(void *pvParam){
-	bool *puntero= (bool*)(pvParam);
-	*puntero =! *puntero;
-}
 
 
 /*==================[external functions definition]==========================*/
@@ -183,8 +259,11 @@ void app_main(void){
 	SwitchesInit();
 	/// defino que cuando se presiona los switch, que se ejecute determinada función, y le mando 
 	/// otro parámetro
-	SwitchActivInt(SWITCH_1,FuncionSwitch, &activar);
-	SwitchActivInt(SWITCH_2,FuncionSwitch, &congelar);
+	SwitchActivInt(SWITCH_1,cambiarEstado, &activar);
+	SwitchActivInt(SWITCH_2,cambiarEstado, &congelar);
+
+	// Congifuracion de puertos:
+	//GPIOInit(pin , dirección: input u  output);
 
 	/* Definición del timer: lo que me hace definir más de un timer es cuando quiero trabajar con diferntes bases de tiempo.
 	Como ahora tengo la misma base de tiempo que es 1 segundo = 1000000 microsegundos, voy a crear solo uno para las mismas tareas*/
@@ -196,14 +275,24 @@ void app_main(void){
 		/// línea anterior. 
         .param_p = NULL
     };
-
-	/// Inicialización del timer
+		/// Inicialización del timer
 	TimerInit(&timer_A);
+
+	serial_config_t Uart = {
+		.port = UART_PC,
+		.baud_rate = 9600,
+		.func_p = FuncionUART,
+		.param_p = NULL
+	};
+	/// Inicializo el UART
+	UartInit(&Uart);
+
 
 	/// creación de las tareas que quiero ejecutar 
 	xTaskCreate(&SensarTask, "Sensar", 512, NULL, 4, &sensar_task_handle);
 	xTaskCreate(&LedsTask, "Leds", 512, NULL, 4, &leds_task_handle);
 	xTaskCreate(&DisplayTask, "Display", 512, NULL, 4, &display_task_handle);
+	xTaskCreate(&MandarDatosUART, "UART", 512, NULL, 4, &MandarDarosUART_handle);
 
 	/// línea donde defino que comienza a funcionar el timer. En este caso, timer_A.timer me devuelve TIMER_A
 	/// porque es de la estructura definida en la línea 179, el primer ítem
