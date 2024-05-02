e35339f7d6d38fc7c6004f01aa02f3f56c83f9a4
GiovannaVinoli <gvinolimiotti@ingenieria.uner.edu.ar>
2024-05-02 08:49:44 -0300

    Se completa documentación del ejercicio 4

--- firmware/projects/guia2_ej4/main/guia2_ej4.c
+++ firmware/projects/guia2_ej4/main/guia2_ej4.c
@@ -41,13 +41,20 @@
 /*==================[macros and definitions]=================================*/
 /**
  * @def valores
- * @brief 
+ * @brief variable que almacena lo que se lee desde el canal CH1
 */
 uint16_t valores;
-int i =0;
+/**
+ * @def contador
+ * @brief variable para controlar el valor del índice del vector de ECG
+*/
+int contador =0;
 
-#define CONFIG_BLINK_PERIOD_uS_A 2000
-#define CONFIG_BLINK_PERIOD_uS_B 4329
+/**
+ * @brief Declaración de los tiempos que utilizan los timers
+*/
+#define CONFIG_PERIOD_uS_A 2000
+#define CONFIG_PERIOD_uS_B 4329
 
 /*==================[internal data definition]===============================*/
 
@@ -55,11 +62,17 @@ void funcionUART(void* param);
 static void conversionADC(void *pParam);
 static void conversionDAC(void *pParam);
 
+/// @brief identificadores de FREERTOS utilizados para hacer referencia a las tareas y así
+/// poder ejecutarlas (a partir de estar referidas por estos TaskHandle) en otros 
+/// comandos de vTask como Delete, Create, etc.
 TaskHandle_t conversion_ADC = NULL;
 TaskHandle_t conversion_DAC = NULL;
-int contador = 0;
-/*==================[internal functions declaration]=========================*/
 
+/*==================[internal functions declaration]=========================*/
+/**
+ * @fn static void conversionADC(void *pParam)
+ * @brief función que lee desde el canal 1 una señal analógica
+*/
 static void conversionADC(void *pParam){
 
 	while(true){
@@ -70,29 +83,46 @@ static void conversionADC(void *pParam){
 	}
 
 }
-
+ /**
+  * @fn static void conversionDAC(void *pParam)
+  * @brief funcion para escribir los valores de ECG como señal analógica
+ */
 static void conversionDAC(void *pParam){
 	while(true){
-		if(i<231){
+		if(contador<BUFFER_SIZE){
 			ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-			AnalogOutputWrite(ecg[i]);
-			i++;
+			AnalogOutputWrite(ecg[contador]);
+			contador++;
 		}
 		else{
-			i = 0;
+			contador = 0;
 		}
 	}
 }
+/**
+ * @fn void funcionUART(void *param)
+ * @brief función definida para configurar la UART
+*/
 void funcionUART(void* param){
 	//ver si tenemos que usarlo para leer algo desde el puerto serie.
 }
-
+/**
+ * @fn void FuncTimerA(void* param)
+ * @brief Función invocada en la interrupción del timer A. Con esta función defino que va 
+ * a ocurrir cuando defino y utilizo el timer. Dentro de ella, creo notificaciones a la 
+ * tarea para realizar la conversion ADC (leer la señal y mostrarla por la UART)
+ */
 void funcionTimerA(void *pParam){
 	//agrego notificaciones
 	vTaskNotifyGiveFromISR(conversion_ADC, pdFALSE);
 
 }
-
+/**
+ * @fn void FuncTimerB(void* param)
+ * @brief Función invocada en la interrupción del timer B. Con esta función defino que va 
+ * a ocurrir cuando defino y utilizo el timer. Dentro de ella, creo notificaciones a la 
+ * tarea conversion_DAC para escribir el ECG
+ */
 void funcionTimerB(void *pParam){
 	//agrego notificaciones
 	vTaskNotifyGiveFromISR(conversion_DAC, pdFALSE);
@@ -100,43 +130,56 @@ void funcionTimerB(void *pParam){
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
-
+	/// inicializo el struct para configurar qué puerto voy a utilizar
+	/// y en qué modo
 	analog_input_config_t Analog_config = {
 		.input = CH1,
 		.mode = ADC_SINGLE
 	};
 
+	/// inicializo el analógico para entrada y salida
 	AnalogInputInit(&Analog_config);
 	AnalogOutputInit();
 
+	/// Definición del timerA: lo que me hace definir más de un timer es cuando quiero trabajar 
+	/// con diferntes bases de tiempo. Como ahora tengo la misma base de tiempo que es 1 
+	/// segundo = 1000000 microsegundos, voy a crear solo uno para las mismas tareas 
 	timer_config_t timerA = {
 		.timer = TIMER_A,
-		.period = CONFIG_BLINK_PERIOD_uS_A,
+		.period = CONFIG_PERIOD_uS_A,
 		.func_p = funcionTimerA,
 		.param_p = NULL
 	};
+	/// inicialización del timerA
 	TimerInit(&timerA);
 
+	/// Definición del timerB: lo que me hace definir más de un timer es cuando quiero trabajar con diferntes bases de tiempo.
+	/// Como ahora tengo la misma base de tiempo que es 1 segundo = 1000000 microsegundos, voy a crear solo uno para las mismas tareas 
 	timer_config_t timerB = {
 		.timer = TIMER_B,
-		.period = CONFIG_BLINK_PERIOD_uS_B,
+		.period = CONFIG_PERIOD_uS_B,
 		.func_p = funcionTimerB,
 		.param_p = NULL
 	};
+	 /// inicialización del timer B
 	TimerInit(&timerB);
 
+	/// configuración de la UART
 	serial_config_t Uart = {
 		.port = UART_PC,
 		.baud_rate = 38400,
 		.func_p = funcionUART,
 		.param_p = NULL
 	};
+
+	/// inicializo la UART
 	UartInit(&Uart);
 
+	/// creación de las tareas que quiero ejecutar 
 	xTaskCreate(&conversionADC, "digitalizar", 512, NULL, 3, &conversion_ADC);
 	xTaskCreate(&conversionDAC, "digital-analogico", 512, NULL, 3, &conversion_DAC);
 
-
+	/// comienza a ejecutarse los timers
 	TimerStart(timerA.timer);
 	TimerStart(timerB.timer);
 
