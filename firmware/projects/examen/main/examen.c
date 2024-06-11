945e34822ee23cc73dd7f92f3afdfd487bc85e32
GiovannaVinoli <gvinolimiotti@ingenieria.uner.edu.ar>
2024-06-11 11:27:37 -0300

    Se crea descripción del proyecto con especificaciones de las conexiones de la placa

--- firmware/projects/examen/main/examen.c
+++ firmware/projects/examen/main/examen.c
@@ -1,23 +1,32 @@
-/*! @mainpage Template
+/*! @mainpage Examen
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
- *
- * <a href="https://drive.google.com/...">Operation Example</a>
+ *  irrigación automática de plantas
+ * Se diseña un dispositivo que controle la humedad y el pH de una plantera. 
+ * Utilizando un sensor de humedad, se accionará o no la bomba de riego.
+ * A partir de un sensor de pH, determinaremos si es necesario aumentarlo o 
+ * disminuirlo, y en base a ello se accionará la bomba de pH (ácido o básico)
+ * que corresponda.
+ * Se requiere que el sistema realice las mediciones cada 3 segundos y que cada 5 
+ * segundos informe sobre el estado del mismo. 
+ * Utilizando las teclas 1 y 2 se podrá encender o apagar el sistema, respectivamente.
  *
  * @section hardConn Hardware Connection
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
- *
+ * | Sensor humedad	| 	GPIO_1		|
+ * | 	Sensor pH	| 	GPIO_3		|
+ * |bomba de agua	| 	GPIO_21		|
+ * | 	bomba pHA	| 	GPIO_22		|
+ * | 	bomba pHB	| 	GPIO_23		| 
  *
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 11/06/2024 | Creación del proyecto 	                         |
  *
  * @author Giovanna Viñoli (giovanna.vinoli@uner.edu.ar)
  *
@@ -34,6 +43,6 @@
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	
 }
 /*==================[end of file]============================================*/
